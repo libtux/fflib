4b09810c6f39cfbb48321726b7a53cf3974ceea0
libtux <tuxaliases@gmail.com>
2025-06-16 16:22:53 +0200

    little fixes

--- target/package/fflib-0.1.0/target/debug/deps/fflib-22ac38dc2d79aa9e.d
+++
@@ -1,7 +0,0 @@
-/home/franek/ff/fflib/target/package/fflib-0.1.0/target/debug/deps/libfflib-22ac38dc2d79aa9e.rmeta: src/lib.rs
-
-/home/franek/ff/fflib/target/package/fflib-0.1.0/target/debug/deps/libfflib-22ac38dc2d79aa9e.rlib: src/lib.rs
-
-/home/franek/ff/fflib/target/package/fflib-0.1.0/target/debug/deps/fflib-22ac38dc2d79aa9e.d: src/lib.rs
-
-src/lib.rs:
--- target/package/fflib-0.1.0/target/debug/libfflib.d
+++
@@ -1,1 +0,0 @@
-/home/franek/ff/fflib/target/package/fflib-0.1.0/target/debug/libfflib.rlib: /home/franek/ff/fflib/target/package/fflib-0.1.0/src/lib.rs
